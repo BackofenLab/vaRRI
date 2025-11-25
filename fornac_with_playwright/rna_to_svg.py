fe8e99bb984ebea5bb68b1f265fea4c44e7f1f2a
fg274 <fabianganter@posteo.de>
2025-11-25 16:45:34 +0100

    added png picture functionallity including png argument parsing

--- fornac_with_playwright/rna_to_svg.py
+++ fornac_with_playwright/rna_to_svg.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 import time
 import argparse
+# playwright version: pytest-playwright-0.7.2
 from playwright.sync_api import sync_playwright
-
+import urllib.parse
+import re
 
 # -----------------------------------------------------------------
 project_dir = Path(__file__).resolve().parent.parent.absolute()
@@ -12,6 +14,7 @@ fornac_css = project_dir / "fornac" / "fornac.css"
 template_barebone_html = project_dir / "example_html" / "template_barebone.html"
 # set the path and create the name of the new svg file
 rna_timestamp_svg = working_dir / ("rna_" + str(time.time()) + ".svg")
+rna_timestamp_png = working_dir / ("rna_" + str(time.time()) + ".png")
 
 
 # -----------------------------------------------------------------
@@ -32,10 +35,10 @@ except Exception as e:
 # -----------------------------------------------------------------
 # open a headless chromium browser instance and load html file with
 # FornaContainer. Extract the created svg into a seperated svg file
-def run(structure, sequence):
+def run(structure, sequence, file_type):
     try:
         with sync_playwright() as p:
-            browser = p.chromium.launch()
+            browser = p.chromium.launch(headless=True)
             page = browser.new_page()
             assert template_barebone_html.exists()
             page.goto("file:///" + str(template_barebone_html))
@@ -49,23 +52,26 @@ def run(structure, sequence):
 					container.addRNA(options.structure, options);
 				}""", [structure, sequence])
             
-			# when making screenshot the text wount display: figuring out the reason
-            # page.evaluate("""() => {document.getElementsByTagName("text")[0].innerHTML="newtext"}""")
-            page.locator("svg").first.screenshot(path="screenshot.png")
-
-
-            #page.screenshot(path="screenshot.png")
-            print(page.content())
 
 			#  extracting the built svg file
             svg = page.locator("svg").first.inner_html()
             # combining all svg file elements in one string
             final_svg = svg_header + svg_style + svg + svg_footer
+                        
+			# svg und png funktionieren auch ohne?
+            # final_svg = re.sub('<title>empty:[0-9]{1,2}</title>', '', final_svg)
+
             
-            
-			# writing the string into a file
-            with open(rna_timestamp_svg, "w") as f:
-                f.write(final_svg)
+            if file_type == "png":
+                # create a new page with the svg code and take a screenshot
+                svg_page = browser.new_page()
+                url_svg = urllib.parse.quote(final_svg)
+                svg_page.goto(f"data:image/svg+xml,{url_svg}")
+                svg_page.screenshot(path=rna_timestamp_png)
+            if file_type == "svg":
+    			# writing the svg code as a string into a file
+                with open(rna_timestamp_svg, "w") as f:
+                    f.write(final_svg)
             
 			# close chromium borwser
             browser.close()
@@ -80,23 +86,32 @@ def run(structure, sequence):
 # sequence | example CGCUUCAUAUAAUCCUAAUGACCUAU
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser(
+    parser = argparse.ArgumentParser(
 			prog='rna_to_svg.py',
 			description='takes as an input a rna structure and sequence and ' \
 			'creates a svg with a grafical representation using fornac' \
 			'example: \n ' \
-			'pthon3.10 rna_to_svg.py -struc "((..((....)).(((....))).))" - seq CGCUUCAUAUAAUCCUAAUGACCUAU')
-	parser.add_argument(
+			'python3.10 rna_to_svg.py "..((((...))))...((...((...((..&............))...))...)).." "ACGAUCAGAGAUCAGAGCAUACGACAGCAG&ACGAAAAAAAGAGCAUACGACAGCAG"')
+    parser.add_argument(
 			'structure',
 			help='structure of the rna, in dot-bracket notation')
-	parser.add_argument(
+    parser.add_argument(
 			'sequence',
 			help='sequence of the rna, out of the letters C,G,U and A')
+    parser.add_argument(
+			'-file_type',
+			help='decide if the picture should be a png or svg',
+            default='svg')
+
+    args = parser.parse_args()
     
-	args = parser.parse_args()
+    if args.structure is None or args.sequence is None:
+        print("Error: a rna structure and sequence is needed")
+        exit()
     
-	if args.structure is None or args.sequence is None:
-		print("Error: a rna structure and sequence is needed")
-		exit()
-    
-	run(args.structure, args.sequence)
+    if len(args.structure) != len(args.sequence):
+        raise Exception("structure and sequence have differnt lengths")
+    if not (args.file_type == "svg" or args.file_type == "png"):
+        raise Exception("file_type is not accepted (only svg and png)")
+   
+    run(args.structure, args.sequence, args.file_type)
