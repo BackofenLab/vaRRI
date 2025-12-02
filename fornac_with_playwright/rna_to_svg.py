5190b7b3277be89c35f464633b60d28b0d4488d9
fg274 <fabianganter@posteo.de>
2025-12-02 10:43:39 +0100

    added the option to sepcify the output file and removed the file_type option

--- fornac_with_playwright/rna_to_svg.py
+++ fornac_with_playwright/rna_to_svg.py
@@ -13,10 +13,8 @@ working_dir = Path(__file__).resolve().parent.absolute()
 # get the path to fornac.css and template_barebone.html
 fornac_css = project_dir / "fornac" / "fornac.css"
 template_barebone_html = project_dir / "example_html" / "template_barebone.html"
-# set the path and create the name of the new svg file
-rna_timestamp_svg = working_dir / ("rna_" + str(time.time()) + ".svg")
-rna_timestamp_png = working_dir / ("rna_" + str(time.time()) + ".png")
-
+# set the path and create the name of the new file without the file type
+path_rna_timestamp = working_dir / ("rna_" + str(time.time()))
 
 # -----------------------------------------------------------------
 # creating the elemnts for the svg file:
@@ -45,9 +43,10 @@ def sequence_coloring(first_seq, second_seq) -> list:
 # -----------------------------------------------------------------
 # open a headless chromium browser instance and load html file with
 # FornaContainer. Extract the created svg into a seperated svg file
-def run(structure, sequence, file_type, coloring_type="default"):
+def run(structure, sequence, file_name, file_type, coloring_type="default"):
     try:
         with sync_playwright() as p:
+            # start browser and load page with fornac script
             browser = p.chromium.launch(headless=True)
             page = browser.new_page()
             assert template_barebone_html.exists()
@@ -98,20 +97,23 @@ def run(structure, sequence, file_type, coloring_type="default"):
             svg = page.locator("svg").first.inner_html()
             # combining all svg file elements in one string
             final_svg = svg_header + svg_style + svg + svg_footer
-                        
-			# svg und png funktionieren auch ohne?
-            # final_svg = re.sub('<title>empty:[0-9]{1,2}</title>', '', final_svg)
 
+            # finalise file name
+            if file_name == "default":
+                path_file = working_dir / ("rna_" + str(time.time()) + "." + file_type)
+            else:
+                path_file = working_dir / (file_name + "." + file_type)
+            
             
             if file_type == "png":
                 # create a new page with the svg code and take a screenshot
                 svg_page = browser.new_page()
                 url_svg = urllib.parse.quote(final_svg)
                 svg_page.goto(f"data:image/svg+xml,{url_svg}")
-                svg_page.screenshot(path=rna_timestamp_png)
+                svg_page.screenshot(path=path_file)
             if file_type == "svg":
     			# writing the svg code as a string into a file
-                with open(rna_timestamp_svg, "w") as f:
+                with open(path_file, "w") as f:
                     f.write(final_svg)
             
 			# close chromium borwser
@@ -141,25 +143,40 @@ if __name__ == '__main__':
 			'sequence',
 			help='sequence of the rna, out of the letters C,G,U and A')
     parser.add_argument(
-			'-file_type',
-			help='decide if the picture should be a png or svg',
-            default='svg')
+            '-o',
+			'--output',
+			help='give the output picture a name and a file type. ' \
+            'Supported are svg and png. Default is svg',
+            default='default.svg')
     parser.add_argument(
-			'-coloring_type',
+            '-c',
+			'--coloring_mode',
 			help='how should the nucleotides be colored?' \
             'default: default coloring fornac' \
             'distinct: each sequence gets its own color',
             default='default')
     args = parser.parse_args()
+
+    output_file = args.output
+
+    if "." not in output_file:
+        output_file += ".svg"
+    # split output file name into the name and the file type
+    output_file_name = "".join(output_file.split(".")[:-1])
+    output_file_type = output_file.split(".")[-1]
+
+    # assert that the output specifies a valid file type
+    if not (output_file_type == "svg" or output_file_type == "png"):
+        raise Exception("the specified file type is not accepted (only svg and png)")
+    # assert that the output specifies a name and a type
+    if (output_file_name == "" or output_file_type == ""):
+        raise Exception("the specified output name is not valid")
     
-    if args.structure is None or args.sequence is None:
-        print("Error: a rna structure and sequence is needed")
-        exit()
+    if args.structure is None or args.sequence is None or args.structure == "" or args.sequence == "":
+        raise Exception("Error: a rna structure and sequence is needed")
     
     if len(args.structure) != len(args.sequence):
         raise Exception("structure and sequence have differnt lengths")
-    if not (args.file_type == "svg" or args.file_type == "png"):
-        raise Exception("file_type is not accepted (only svg and png)")
-    if not (args.coloring_type == "default" or args.coloring_type == "distinct"):
-        raise Exception("coloring_type is not accepted (only default and distinct)")
-    run(args.structure, args.sequence, args.file_type, args.coloring_type)
+    if not (args.coloring_mode == "default" or args.coloring_mode == "distinct"):
+        raise Exception("coloring_mode is not accepted (only default and distinct)")
+    run(args.structure, args.sequence, output_file_name, output_file_type, args.coloring_mode)
