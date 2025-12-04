817857e74335fadbc92994a3fd0ef13ff76f9bf3
fg274 <fabianganter@posteo.de>
2025-12-05 00:44:12 +0100

    removed old folders, fixed indexing problem, added indexing offset functionality, added extra interbasepair visibility

--- fornac_with_playwright/rna_to_svg.py
+++ fornac_with_playwright/rna_to_svg.py
@@ -38,12 +38,10 @@ def sequence_coloring(first_seq, second_seq) -> list:
     return color
 
 
-
-
 # -----------------------------------------------------------------
 # open a headless chromium browser instance and load html file with
 # FornaContainer. Extract the created svg into a seperated svg file
-def run(structure, sequence, file_name, file_type, coloring_type="default"):
+def run(structure, sequence, file_name, file_type, coloring_type="default", offset_1 = 0, offset_2 = 0):
     try:
         with sync_playwright() as p:
             # start browser and load page with fornac script
@@ -61,33 +59,69 @@ def run(structure, sequence, file_name, file_type, coloring_type="default"):
 					container.addRNA(options.structure, options);
 				}""", [structure, sequence])
 
-#
+            # -----------------------------------------------------
+            # changing the background color
             coloring = []
+            first_seq = sequence.split("&")[0]
+            second_seq = sequence.split("&")[1] if "&" in sequence else ""
             # this option colors all nucleotides of one sequence in one color
             if coloring_type == "distinct":
-                first_seq = sequence.split("&")[0]
-                second_seq = sequence.split("&")[1] if "&" in sequence else ""
                 coloring = sequence_coloring(first_seq, second_seq)
             
             # color all circles with the given color in the coloring list
             page.evaluate("""(coloring) => {
                     function coloringTheCircles(coloring) {
                         if (coloring.length == 0) {return;}
-                        var list_of_nodes = document.getElementsByClassName("fornac-node");
-                        for (const [index, node] of Object.entries(list_of_nodes)){
-                            if (node.getAttribute("r") == "5"){
-                            if (coloring.length < index) {
-                            document.getElementById("debug").innerHTML += 
-                            'index out of color array bounds: ' + index.toString() + '|';
-                            continue;
-                            }
+                        var list_of_nodes = document.querySelectorAll('[r="5"]');
+                        for (const [index, node] of Object.entries(list_of_nodes)){            
                             node.setAttribute("style", "fill: " + coloring[index] + ";");
-                            }    
                         }
                     }
                     coloringTheCircles(coloring)
 				}""", coloring)          
-             
+            
+
+            # -----------------------------------------------------
+            # changing the indexing
+            indexing = []
+            # index for the first sequence
+            indexing = [str(i) for i in range(10 + offset_1, len(first_seq)+1+offset_1, 10)]
+            # indexing for the second sequence
+            first_index = 8 - (len(first_seq) % 10) + offset_2
+            indexing += [str(i) for i in range(first_index, len(second_seq)+1+offset_2, 10)]
+
+            page.evaluate("""(indexing) => {
+                    var list_of_text_elements = document.querySelectorAll('[label_type="label"]');
+                    for (const [index, node] of Object.entries(list_of_text_elements)){
+                        node.innerHTML = indexing[index];                            
+                    }
+				}""", indexing)
+
+            # -----------------------------------------------------
+            # changing the basepair ring color
+            end_of_seq1 = len(first_seq)
+            page.evaluate("""(end_of_seq1) => {
+                    var list_of_basepair_links = document.querySelectorAll('[link_type="basepair"]');
+                    var basepair_indicies = [];
+                    for (const [index, node] of Object.entries(list_of_basepair_links)){
+                        text = node.children[0].innerHTML;
+                        basepairs = text.split(":")[1];
+                        basepair_node_1 = basepairs.split("-")[0];
+                        basepair_node_2 = basepairs.split("-")[1];
+                        if (basepair_node_1 < end_of_seq1 && basepair_node_2 > end_of_seq1) {                          
+                            basepair_indicies.push(basepair_node_1);
+                            basepair_indicies.push(basepair_node_2);
+                        }
+                    }
+                          
+                    for (const index of basepair_indicies){
+                        node = document.querySelector('circle[node_num="' + index.toString() + '"]'); 
+                        style = node.getAttribute("style");
+                        node.setAttribute("style", style + "stroke: red;");
+                    }
+				}""", end_of_seq1)          
+
+
             # debugging:
             debug_text = page.locator("div").first.inner_html()
             print(debug_text)
@@ -155,6 +189,16 @@ if __name__ == '__main__':
             'default: default coloring fornac' \
             'distinct: each sequence gets its own color',
             default='default')
+    parser.add_argument(
+			'--offset_first',
+			help='with what offset should the indexing of the first sequence start?' \
+            'default: 0',
+            default="0")
+    parser.add_argument(
+			'--offset_second',
+			help='with what offset should the indexing of the second sequence start if ther is one?' \
+            'default: 0',
+            default="0")
     args = parser.parse_args()
 
     output_file = args.output
@@ -164,6 +208,9 @@ if __name__ == '__main__':
     # split output file name into the name and the file type
     output_file_name = "".join(output_file.split(".")[:-1])
     output_file_type = output_file.split(".")[-1]
+ 
+    sequence = args.sequence
+    structure = args.structure
 
     # assert that the output specifies a valid file type
     if not (output_file_type == "svg" or output_file_type == "png"):
@@ -179,4 +226,14 @@ if __name__ == '__main__':
         raise Exception("structure and sequence have differnt lengths")
     if not (args.coloring_mode == "default" or args.coloring_mode == "distinct"):
         raise Exception("coloring_mode is not accepted (only default and distinct)")
-    run(args.structure, args.sequence, output_file_name, output_file_type, args.coloring_mode)
+    if (not args.offset_first.isdigit()) or (not args.offset_second.isdigit()):
+        raise Exception("index starting positions must be a number of type integer")
+    offset_1 = int(args.offset_first)
+    offset_2 = int(args.offset_second)
+
+    # assert that there are as many "(" as ")"
+    if structure.count("(") != structure.count(")"):
+        raise Exception("the structure is invalid. The number of ( and ) must be the same")  
+
+
+    run(structure, sequence, output_file_name, output_file_type, args.coloring_mode, offset_1, offset_2)
