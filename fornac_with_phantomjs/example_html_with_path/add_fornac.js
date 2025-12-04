a252337f7e8c496a17f98da395be97ae2d32ad0e
fg274 <fabianganter@posteo.de>
2025-12-05 00:49:56 +0100

    removed old files

--- fornac_with_phantomjs/example_html_with_path/add_fornac.js
+++
@@ -1,7 +0,0 @@
-var container = new fornac.FornaContainer("#rna_ss", {'animation': false});
-
-var options = {'structure': '((..((....)).(((....))).))',
-                        'sequence': 'CGCUUCAUAUAAUCCUAAUGACCUAU'
-};
-
-container.addRNA(options.structure, options);
--- fornac_with_phantomjs/testing_phantom/execute_test.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("test").innerHTML = "this is a test";
-document.getElementById("test").setAttribute('works', 'Right')
-if (test_variable == false) {
-    console.log("no access to global variable test_variable");
-}
